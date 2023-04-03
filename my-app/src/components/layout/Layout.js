9255f7395bb62d1121a6134151494acc42f68fa8
Steven Miller <millersteven@live.com>
2023-04-03 10:25:50 -0400

    Added simple layout

---
+++ my-app/src/components/layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import App from '../../App'
+
+function Layout() {
+  return (
+    <>
+    <Header></Header>
+    <App></App>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default Layout
